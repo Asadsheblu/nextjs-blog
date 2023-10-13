ca38bf6237c514acb85bb5d7b8fbc6c0518d8d08
Asadsheblu <shebluasad@gmail.com>
2023-10-13 09:19:51 -0700

    error-solve

--- pages/Home/Review.js
+++ pages/Home/Review.js
@@ -1,168 +1,198 @@
-import Image from 'next/image';
-import React from 'react';
-import { StarFill, StarHalf } from 'react-bootstrap-icons';
+import Image from "next/image";
+import React from "react";
+import { StarFill, StarHalf } from "react-bootstrap-icons";
 
-import Slider from "react-slick"
-import avatarMale from "../../public/img/img_avatar.png"
-import avatarFemale from "../../public/img/img_avatar2.png"
+import Slider from "react-slick";
+import avatarMale from "../../public/img/img_avatar.png";
+import avatarFemale from "../../public/img/img_avatar2.png";
 const Review = () => {
-    const sliderSettings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        initialSlide: 0,
-    
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: false,
-              dots: true,
-            },
-          },
-        ],
-      };
-    return (
-        <div id="review" className="container-fluid bg-light">
+  const sliderSettings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
 
-        <h2 className="text-center headline fw-bold p-3">Our Client's Say</h2>
-       
-        <div className=" pb-5 container">
-          
-                
-                <Slider {...sliderSettings}>
-                    <div className="shadow rounded bg-white text-center p-4">
-                        <div className="text-center">
-                           
-                            <Image src={avatarMale} className="w-25 h-25 client-img" alt="client-img"/>
-                        </div>
-                        <div className="pt-3 text-center">
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, sint!</p>
-                            <div className="text-warning">
-                            <StarFill/>
-                              <StarFill/>
-                              <StarFill/>
-                              <StarFill/>
-                              <StarHalf/>
-                            </div>
-                            <div className="pt-2 fw-bold">
-                                <i className="bi bi-dash"></i>
-                                <span>John Smith</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="shadow rounded   bg-white  text-center p-4">
-                        <div className="text-center">
-                            <Image src={avatarFemale}className="w-25 h-25 client-img" alt="client-img"/>
-                        </div>
-                        <div className="pt-3 text-center">
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, sint!</p>
-                            <div className="text-warning">
-                            <StarFill/>
-                              <StarFill/>
-                              <StarFill/>
-                              <StarFill/>
-                              <StarHalf/>
-                            </div>
-                            <div className="pt-2 fw-bold">
-                                <i className="bi bi-dash"></i>
-                                <span>Arita Saha</span>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div className="shadow rounded bg-white  text-center p-4">
-                        <div className="text-center">
-                            <Image src={avatarMale} className="w-25 h-25 client-img" alt="client-img"/>
-                        </div>
-                        <div className="pt-3 text-center">
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, sint!</p>
-                            <div className="text-warning">
-                            <StarFill/>
-                              <StarFill/>
-                              <StarFill/>
-                              <StarFill/>
-                              <StarHalf/>
-                            </div>
-                            <div className="pt-2 fw-bold">
-                                <i className="bi bi-dash"></i>
-                                <span>David watson</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="shadow rounded bg-white text-center p-4">
-                        <div className="text-center">
-                            <Image src={avatarFemale} className="w-25 h-25 client-img" alt="client-img" />
-                        </div>
-                        <div className="pt-3 text-center">
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, sint!</p>
-                            <div className="text-warning">
-                            <StarFill/>
-                              <StarFill/>
-                              <StarFill/>
-                              <StarFill/>
-                              <StarHalf/>
-                            </div>
-                            <div className="pt-2 fw-bold">
-                                <i className="bi bi-dash"></i>
-                                <span>Alegabeth Dues</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="shadow rounded bg-white text-center p-4">
-                        <div className="text-center">
-                            <Image src={avatarMale} className="w-25 h-25 client-img" alt="client-img" />
-                        </div>
-                        <div className="pt-3 text-center">
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, sint!</p>
-                            <div className="text-warning">
-                            <StarFill/>
-                              <StarFill/>
-                              <StarFill/>
-                              <StarFill/>
-                              <StarHalf/>
-                            </div>
-                            <div className="pt-2 fw-bold">
-                                <i className="bi bi-dash"></i>
-                                <span>Nure Tapos</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="shadow rounded bg-white  text-center p-4">
-                        <div className="text-center">
-                            <Image src={avatarFemale} className="w-25 h-25 client-img" alt="client-img" />
-                        </div>
-                        <div className="pt-3 text-center">
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, sint!</p>
-                            <div className="text-warning">
-                            <StarFill/>
-                              <StarFill/>
-                              <StarFill/>
-                              <StarFill/>
-                              <StarHalf/>
-                            </div>
-                            <div className="pt-2 fw-bold">
-                                <i className="bi bi-dash"></i>
-                                <span>Anuska Sharma</span>
-                            </div>
-                        </div>
-                    </div>
-                    </Slider>
-                  
-                   
-                
-                
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true,
+        },
+      },
+    ],
+  };
+  return (
+    <div id="review" className="container-fluid bg-light">
+      <h2 className="text-center headline fw-bold p-3">Our Client  Say</h2>
 
-                </div>
-
-
+      <div className=" pb-5 container">
+        <Slider {...sliderSettings}>
+          <div className="shadow rounded bg-white text-center p-4">
+            <div className="text-center">
+              <Image
+                src={avatarMale}
+                className="w-25 h-25 client-img"
+                alt="client-img"
+              />
             </div>
-   
-    );
+            <div className="pt-3 text-center">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Ducimus, sint!
+              </p>
+              <div className="text-warning">
+                <StarFill />
+                <StarFill />
+                <StarFill />
+                <StarFill />
+                <StarHalf />
+              </div>
+              <div className="pt-2 fw-bold">
+                <i className="bi bi-dash"></i>
+                <span>John Smith</span>
+              </div>
+            </div>
+          </div>
+          <div className="shadow rounded   bg-white  text-center p-4">
+            <div className="text-center">
+              <Image
+                src={avatarFemale}
+                className="w-25 h-25 client-img"
+                alt="client-img"
+              />
+            </div>
+            <div className="pt-3 text-center">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Ducimus, sint!
+              </p>
+              <div className="text-warning">
+                <StarFill />
+                <StarFill />
+                <StarFill />
+                <StarFill />
+                <StarHalf />
+              </div>
+              <div className="pt-2 fw-bold">
+                <i className="bi bi-dash"></i>
+                <span>Arita Saha</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="shadow rounded bg-white  text-center p-4">
+            <div className="text-center">
+              <Image
+                src={avatarMale}
+                className="w-25 h-25 client-img"
+                alt="client-img"
+              />
+            </div>
+            <div className="pt-3 text-center">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Ducimus, sint!
+              </p>
+              <div className="text-warning">
+                <StarFill />
+                <StarFill />
+                <StarFill />
+                <StarFill />
+                <StarHalf />
+              </div>
+              <div className="pt-2 fw-bold">
+                <i className="bi bi-dash"></i>
+                <span>David watson</span>
+              </div>
+            </div>
+          </div>
+          <div className="shadow rounded bg-white text-center p-4">
+            <div className="text-center">
+              <Image
+                src={avatarFemale}
+                className="w-25 h-25 client-img"
+                alt="client-img"
+              />
+            </div>
+            <div className="pt-3 text-center">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Ducimus, sint!
+              </p>
+              <div className="text-warning">
+                <StarFill />
+                <StarFill />
+                <StarFill />
+                <StarFill />
+                <StarHalf />
+              </div>
+              <div className="pt-2 fw-bold">
+                <i className="bi bi-dash"></i>
+                <span>Alegabeth Dues</span>
+              </div>
+            </div>
+          </div>
+          <div className="shadow rounded bg-white text-center p-4">
+            <div className="text-center">
+              <Image
+                src={avatarMale}
+                className="w-25 h-25 client-img"
+                alt="client-img"
+              />
+            </div>
+            <div className="pt-3 text-center">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Ducimus, sint!
+              </p>
+              <div className="text-warning">
+                <StarFill />
+                <StarFill />
+                <StarFill />
+                <StarFill />
+                <StarHalf />
+              </div>
+              <div className="pt-2 fw-bold">
+                <i className="bi bi-dash"></i>
+                <span>Nure Tapos</span>
+              </div>
+            </div>
+          </div>
+          <div className="shadow rounded bg-white  text-center p-4">
+            <div className="text-center">
+              <Image
+                src={avatarFemale}
+                className="w-25 h-25 client-img"
+                alt="client-img"
+              />
+            </div>
+            <div className="pt-3 text-center">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Ducimus, sint!
+              </p>
+              <div className="text-warning">
+                <StarFill />
+                <StarFill />
+                <StarFill />
+                <StarFill />
+                <StarHalf />
+              </div>
+              <div className="pt-2 fw-bold">
+                <i className="bi bi-dash"></i>
+                <span>Anuska Sharma</span>
+              </div>
+            </div>
+          </div>
+        </Slider>
+      </div>
+    </div>
+  );
 };
 
 export default Review;
